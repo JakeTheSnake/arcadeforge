fbd865135dbad673ea0af05959f100a4e92b95fb
Jake <jacobo.strm@gmail.com>
2013-11-18 20:23:09 +0100

    Secret token

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Arcadeforge::Application.config.secret_key_base = 'e17955ba2b0fb793d4775ad821b30e011002d7a77765c7e5f79b25ed9f888845d3c72df980222c709d80536f7f7cbb121c266e8f8ffb09ba7781982a2e7ead41'
