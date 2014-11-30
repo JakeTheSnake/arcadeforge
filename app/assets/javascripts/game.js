a6a4e1d282a0515b22799882f9c86ea2a2f5578a
Jake <jacobo.strm@gmail.com>
2014-11-30 18:25:26 +0100

    Added game layout

---
+++ app/assets/javascripts/game.js
@@ -0,0 +1,38 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+
+//= require doubleball/engine/gameCreator.js
+//= require doubleball/engine/componentFunctions.js
+//= require doubleball/engine/scene.js
+//= require doubleball/startup_engine_only.js
+//= require doubleball/engine/helpers.js
+//= require doubleball/ui/htmlStrings.js
+//= require doubleball/engine/baseObject.js
+//= require doubleball/engine/actions.js
+//= require doubleball/engine/freeObject.js
+//= require doubleball/engine/routeObject.js
+//= require doubleball/engine/mouseObject.js
+//= require doubleball/engine/platformObject.js
+//= require doubleball/engine/sceneObject.js
+//= require doubleball/engine/topDownObject.js
+//= require doubleball/engine/timerHandler.js
+//= require doubleball/engine/counter.js
+//= require doubleball/engine/counterObjectImage.js
+//= require doubleball/engine/counterObjectText.js
+
+//= require doubleball/engine/objectEffects.js
+//= require doubleball/engine/events.js
+
---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,24 @@
+class GamesController < ApplicationController
+  layout "game"
+
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  def index
+  end
+
+  def show
+  end
+end
