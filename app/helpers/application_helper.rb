d1ade412efaa8d576bc27fca9b94f4a96abe39d0
Amir Ossanloo <amir@amirossanloo.com>
2015-01-22 18:24:10 +0100

    CSS reworked

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,11 +8,11 @@ module ApplicationHelper
 
 	def account_nav
 		if user_signed_in?
-          [link_to("Profile", edit_user_registration_path), 
-          button_to("Log out", destroy_user_session_path, class: "btn btn-warning", method: :delete)]
+        	[link_to("Profile", edit_user_registration_path), 
+        	link_to("Log out", destroy_user_session_path, class: "btn warning", method: :delete)]
         else
-          [link_to("Login", new_user_session_path),
-        	button_to("Sign up", new_user_registration_path, class: "btn", id: "btn-signup", :method => :get)]
+        	[link_to("Login", new_user_session_path),
+        	link_to("Sign up", new_user_registration_path, class: "btn success", id: "btn-signup", :method => :get)]
         end
     end
 end
