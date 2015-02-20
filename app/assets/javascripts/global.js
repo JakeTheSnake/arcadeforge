e592c4ae026b25e3ba0b31d6fb7e78be13bc98cf
Jake <jacobo.strm@gmail.com>
2015-02-20 23:38:45 +0100

    Additional labels fix

--- app/assets/javascripts/global.js
+++ app/assets/javascripts/global.js
@@ -1,24 +1,28 @@
-$(document).ready(function() {
-    $(".input-container input").each(function() {
-        var textInput = $(this);
-        textInput.on('focus', function() {
-            $(this).parent().addClass('input-container-active');
-            $(this).removeAttr('placeholder');
-        });
+(function() {
+    var readyFunc = function() {
+        $(".input-container input").each(function() {
+            var textInput = $(this);
+            textInput.on('focus', function() {
+                $(this).parent().addClass('input-container-active');
+                $(this).removeAttr('placeholder');
+            });
 
-        textInput.on('blur', function() {
-            if ($(this).val().length == 0) {
+            textInput.on('blur', function() {
+                if ($(this).val().length == 0) {
+                    $(this).parent().removeClass('input-container-active');
+                    $(this).attr('placeholder', textInput.parent().find('label').html());
+                }
+            });
+
+            if (textInput.val().length != 0) {
+                $(this).parent().addClass('input-container-active');
+                $(this).removeAttr('placeholder');
+            } else {
                 $(this).parent().removeClass('input-container-active');
                 $(this).attr('placeholder', textInput.parent().find('label').html());
             }
         });
-
-        if (textInput.val().length != 0) {
-            $(this).parent().addClass('input-container-active');
-            $(this).removeAttr('placeholder');
-        } else {
-            $(this).parent().removeClass('input-container-active');
-            $(this).attr('placeholder', textInput.parent().find('label').html());
-        }
-    });
-});
+    };
+    $(document).ready(readyFunc);
+    $(document).on('page:load', readyFunc);
+})();
