67e0358e0a9924e0aea3e7bfb628ba0e853e6430
Jake <jacobo.strm@gmail.com>
2015-09-15 22:00:24 +0200

    Fixed forgot password URL.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'arcadeforge.io', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'arcadeforge.io' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
