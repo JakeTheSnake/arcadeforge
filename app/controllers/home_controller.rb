95cf71ade6221d2714a344e1b3960213cb92ae5d
AmirOssanloo <amir@amirossanloo.com>
2015-11-04 20:26:31 -0500

    Changed Icons and prepared files for visual framework development

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
+	layout "about", :except => [:index]
   def index
     @featured_games = Game.includes(:user).not_private.featured.limit(5).select_without_data
+    
   end
 
   def about
